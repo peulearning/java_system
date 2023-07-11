043500cc1ecaa0577f6a679c439939e2ddf7573f
Pedro <peuhenry2018@gmail.com>
2023-07-11 20:48:11 -0300

    Inicializando Sistema

---
+++ src/main/java/br/edu/ifnmg/dsc/extractnorth/ExtractnorthApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifnmg.dsc.extractnorth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtractnorthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtractnorthApplication.class, args);
+	}
+
+}
